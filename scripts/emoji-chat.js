ec58591ce29f1f087d9e6fbc903c7fd1575d5423
Kartik Verma <kartx111@gmail.com>
2016-09-06 18:13:25 +0530

    fix: prevent defaultAction on ENTER keyPress and retain cursor position

--- scripts/emoji-chat.js
+++ scripts/emoji-chat.js
@@ -11,13 +11,21 @@ window.onload = function() {
 
   document.getElementById('textarea').onkeyup = function() {
     for (var i in map) {
+      // store current positions in variables
+      var start = this.selectionStart,
+          end = this.selectionEnd;
+
       var regex = new RegExp(escapeSpecialChars(i), 'gim');
       this.value = this.value.replace(regex, map[i]);
+
+      // restore from variables...
+      this.setSelectionRange(start, end);
     }
   };
 
   document.getElementById('textarea').onkeypress = function(e) {
     if (e.which == 13) {
+      e.preventDefault();
       var chatMessage = this.value;
       var div = document.getElementById('chat');
       div.innerHTML = div.innerHTML + chatMessage + "<br/>";
