c9502cb55e840524e77aa3ed6c7e9a5b63c7e2c6
Kartik Verma <kartx111@gmail.com>
2016-09-06 18:17:57 +0530

    feat: Add some more Emoji to the Map

--- scripts/emoji-chat.js
+++ scripts/emoji-chat.js
@@ -3,6 +3,12 @@ window.onload = function() {
   var map = {
     "<3": "\u2764\uFE0F",
     ":)": "\uD83D\uDE03",
+    ":D": "\uD83D\uDE00",
+    ";)": "\uD83D\uDE09",
+    ":(": "\uD83D\uDE12",
+    ":p": "\uD83D\uDE1B",
+    ";p": "\uD83D\uDE1C",
+    ":'(": "\uD83D\uDE22"
   };
 
   function escapeSpecialChars(regex) {
